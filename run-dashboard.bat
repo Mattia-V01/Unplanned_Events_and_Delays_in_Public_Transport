c4546f0c3b668ed33e489b840f745747f126fcde
Mattia-V01 <128465465+Mattia-V01@users.noreply.github.com>
2025-05-26 22:16:03 +0200

    Update run-dashboard.bat

--- run-dashboard.bat
+++ run-dashboard.bat
@@ -1,3 +1,4 @@
 @echo off
-docker run --rm -p 8000:8000 shiny-dashboard
+start http://127.0.0.1:8000
+docker run -it --rm -p 8000:8000 -w /app/App shiny-dashboard uvicorn app:fastapi_app --host 0.0.0.0 --port 8000 --reload
 pause
