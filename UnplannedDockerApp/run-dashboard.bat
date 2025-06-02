1393b8b8e1663c0ddf0c518a7d550713cdd47db5
Mattia Verzaroli <mattia.verzaroli@students.fhnw.ch>
2025-06-02 21:59:19 +0200

    docker

--- UnplannedDockerApp/run-dashboard.bat
+++ UnplannedDockerApp/run-dashboard.bat
@@ -1,4 +1,3 @@
 @echo off
-start http://127.0.0.1:8000
-docker run -it --rm -p 8000:8000 -w /app/App shiny-dashboard uvicorn app:fastapi_app --host 0.0.0.0 --port 8000 --reload
-pause
+start "" http://localhost:8000
+docker run --rm -it -p 8000:8000 -e PYTHONPATH=/app/Unplanned_Events_and_Delays_in_Public_Transport/App transport-delays uvicorn app:fastapi_app --host 0.0.0.0 --port 8000
