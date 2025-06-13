3e4da81be231380c6f52c2c10a5565d9b955da5b
Mattia Verzaroli <mattia.verzaroli@students.fhnw.ch>
2025-06-13 12:28:08 +0200

    bat file

--- UnplannedDockerApp/run-dashboard.bat
+++ UnplannedDockerApp/run-dashboard.bat
@@ -1,3 +1,3 @@
 @echo off
 start "" http://localhost:8000
-docker run --rm -it -p 8000:8000 -e PYTHONPATH=/app/Unplanned_Events_and_Delays_in_Public_Transport/App transport-delays uvicorn app:fastapi_app --host 0.0.0.0 --port 8000
+docker run --rm -it -p 8000:8000 ghcr.io/mattia-v01/unplanned-delays-dashboard:latest
