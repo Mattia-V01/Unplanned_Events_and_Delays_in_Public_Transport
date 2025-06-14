f6f8671ee03d19e7362cebab74f93aa2a911a5c2
Mattia Verzaroli <mattia.verzaroli@students.fhnw.ch>
2025-06-14 10:25:28 +0200

    run-dashboard

--- run-dashboard.bat
+++ run-dashboard.bat
@@ -1,20 +1,8 @@
 @echo off
 echo Starting the dashboard container...
-echo.
-
-:: Check if Docker is running
-docker info >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    echo ERROR: Docker is not running or not installed.
-    pause
-    exit /b 1
-)
-
-:: Wait 3 seconds before opening the browser
-timeout /t 3 /nobreak >nul
 start "" http://localhost:8000
-
-:: Run the Docker image
-docker run --rm -it -p 8000:8000 ghcr.io/mattia-v01/unplanned-delays-dashboard:latest
-
-pause
+docker run --rm -it -p 8000:8000 ^
+  -v "%~dp0.":/app ^
+  -w /app/App ^
+  ghcr.io/mattia-v01/unplanned-delays-dashboard:latest ^
+  uvicorn app:fastapi_app --host 0.0.0.0 --port 8000
